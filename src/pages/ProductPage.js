9c135632e4437d2e482e3741bb0d6ce0e96c74c8
aratodaniel <aratodana1@gmail.com>
2023-10-19 19:48:34 +0200

    Add currency format

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -3,53 +3,54 @@ import { useTranslation } from "react-i18next";
 import style from "./ProductPage.module.scss";
 
 // Mocked productList data
+// TODO: use currencyFormatter to format price
 const products= [
   {
     id: 1,
     title: 'Poly Edge 550',
-    price: 30,
+    price: '£30',
     image: require('../assets/products/item1.png')
   },
   {
     id: 2,
     title: 'Poly Edge E320',
-    price: 30,
+    price: '£30',
     image: require('../assets/products/item2.png')
   },
   {
     id: 3,
     title: 'Poly Edge B30',
-    price: 210,
+    price: '£210',
     image: require('../assets/products/item3.png')
   },
   {
     id: 4,
     title: 'Nokia 3210',
-    price: 2010,
+    price: '£2,010',
     image: require('../assets/products/item4.png')
   },
   {
     id: 5,
     title: 'Blackwire 52xx',
-    price: 29,
+    price: '£29',
     image: require('../assets/products/item5.png')
   },
   {
     id: 6,
     title: 'Voyager 43xx',
-    price: 29,
+    price: '£29',
     image: require('../assets/products/item6.png')
   },
   {
     id: 7,
     title: 'Savi 72xx',
-    price: 29,
+    price: '£29',
     image: require('../assets/products/item7.png')
   },
   {
     id: 8,
     title: 'Voyager Free 60',
-    price: 29,
+    price: '£29',
     image: require('../assets/products/item8.png')
   },
 ]
