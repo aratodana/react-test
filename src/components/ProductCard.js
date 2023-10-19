dfe7e3be71c2463abb43d70f43c45de0ae4ae7e7
aratodaniel <aratodana1@gmail.com>
2023-10-19 02:43:10 +0200

    Add product-image

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,7 +2,15 @@ import style from "./ProductCard.module.scss";
 function productCard(props) {
   return (
     <div className={style.productCard}>
-        asdf
+        <div className={style.productCardImageWrapper}>
+            <img src={props.product.image} alt={props.product.title} className={style.productCardImage} />
+        </div>
+      <h3 className={style.productCardTitle}>
+        {props.product.title}
+      </h3>
+      <div className={style.productCardPrice}>
+          { props.product.price }
+      </div>
     </div>
   );
 }
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -2,18 +2,68 @@ import { Banner, ProductCard, Spring } from "../components";
 import { useTranslation } from "react-i18next";
 import style from "./ProductPage.module.scss";
 
+// Mocked productList data
+const products= [
+  {
+    id: 1,
+    title: 'Poly Edge 550',
+    price: 30,
+    image: require('../assets/products/item1.png')
+  },
+  {
+    id: 2,
+    title: 'Poly Edge E320',
+    price: 30,
+    image: require('../assets/products/item2.png')
+  },
+  {
+    id: 3,
+    title: 'Poly Edge B30',
+    price: 210,
+    image: require('../assets/products/item3.png')
+  },
+  {
+    id: 4,
+    title: 'Nokia 3210',
+    price: 2010,
+    image: require('../assets/products/item4.png')
+  },
+  {
+    id: 5,
+    title: 'Blackwire 52xx',
+    price: 29,
+    image: require('../assets/products/item5.png')
+  },
+  {
+    id: 6,
+    title: 'Voyager 43xx',
+    price: 29,
+    image: require('../assets/products/item6.png')
+  },
+  {
+    id: 7,
+    title: 'Savi 72xx',
+    price: 29,
+    image: require('../assets/products/item7.png')
+  },
+  {
+    id: 8,
+    title: 'Voyager Free 60',
+    price: 29,
+    image: require('../assets/products/item8.png')
+  },
+]
+
+
 function ProductPage() {
   const { t } = useTranslation();
   return <div>
     <Banner title={t('pages.product_list.banner.title')}></Banner>
     <Spring>
       <div className={style.productList}>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
+        {products.map((product) => (
+            <ProductCard  product={product} key={product.id}></ProductCard>
+        ))}
       </div>
     </Spring>
   </div>;
